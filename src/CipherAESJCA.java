3b67bb06bc3a45a149c78af6038727f65c8e3e36
Han-gyeol Kim <83993373+ocxh@users.noreply.github.com>
2023-01-21 00:03:54 +0900

    Cipher-AES(2)

--- src/CipherAESJCA.java
+++ src/CipherAESJCA.java
@@ -13,20 +13,29 @@ public class Main {
         SecretKey secretKey = keyGenerator.generateKey();
         //암호화
         byte[] encryptData = encrypt(secretKey, plainText.getBytes());
-        System.out.println("Encrypted: "+new String(encryptData));
+        print_bytes(encryptData);
 
         //복호화
         byte[] decryptData = decrypt(secretKey, encryptData);
         System.out.println("Decrypted: "+new String(decryptData));
     }
+    public static void print_bytes(byte[] bArr){
+        StringBuilder builder = new StringBuilder();
+
+        for (byte data : bArr) {
+            builder.append(String.format("%02X ", data));
+        }
+
+        System.out.println("Encrypted: "+builder.toString());
+    }
     public static byte[] encrypt(SecretKey secretKey, byte[] plainText) throws GeneralSecurityException{
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Cipher cipher = Cipher.getInstance("AES");
         cipher.init(1, secretKey);
         byte[] cipherText = cipher.doFinal(plainText);
         return cipherText;
     }
     public static byte[] decrypt(SecretKey secretKey, byte[] cipherText) throws GeneralSecurityException{
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Cipher cipher = Cipher.getInstance("AES");
         cipher.init(2, secretKey);
         byte[] plainText = cipher.doFinal(cipherText);
         return plainText;
